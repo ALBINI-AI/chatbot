86a2ac69d17ec3bed9b393f094b5ba40797693ff
ALBINI_AI <albiniai.2002@gmail.com>
2023-03-10 17:41:22 +0530

    Initial commit

---
+++ message.php
@@ -0,0 +1,26 @@
+<!-- Created By CodingNepal -->
+<?php
+// connecting to database
+$conn = mysqli_connect("localhost", "root", "", "chatbot") or die("Database Error");
+// getting user message through ajax
+$getMesg = mysqli_real_escape_string($conn, $_POST['text']);
+//checking user query to database query
+$check_data = "SELECT replies FROM bot1 WHERE queries LIKE '%$getMesg%'";
+$run_query = mysqli_query($conn, $check_data) or die("Error");
+// if user query matched to database query we'll show the reply otherwise it go to else statement
+if(mysqli_num_rows($run_query) > 0){
+    //fetching replay from the database according to the user query
+    $fetch_data = mysqli_fetch_assoc($run_query);
+    //storing replay to a varible which we'll send to ajax
+    $replay = $fetch_data['replies'];
+    
+    if($replay=="Thank you😉"){
+        echo '<a href="index.html">End chat</a> ';
+    }
+    else{
+        echo $replay;
+    }
+}else{
+    echo "Sorry can't be able to understand you!";
+}
+?>
